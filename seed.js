ffaba9a75faac6f26e0e91d6fa5af4b329640d90
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2023-11-06 10:17:45 +0000

    Remove unnecessary shit

--- seed.js
+++ seed.js
@@ -4,7 +4,6 @@ const csvToObj = require("./csvToObj");
 const { Comment, Post, sequelize } = require("./db");
 
 async function seed() {
-	// Drops the tables if they already exist
 	await sequelize.sync({ force: true });
 
 	const posts = await csvToObj("./csv/posts.csv");
@@ -14,10 +13,6 @@ async function seed() {
 	const comments = await csvToObj("./csv/comments.csv");
 	await Comment.bulkCreate(comments);
 	console.log("\nComments created");
-
-	// This technically isn't needed
-	await sequelize.close();
-	console.log("\nConnection closed");
 }
 
 seed();
